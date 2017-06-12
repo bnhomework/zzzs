345706094b368737759fe1094162b7c56817e4a2
sijianbn@hotmail.com <sijianbn@hotmail.com>
2017-06-13 07:40:35 +0800

    xxx

---
+++ BnWS/BnWS/Controllers/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BnWS.Controllers
+{
+    public class ShopController : BaseController
+    {
+        //
+        // GET: /Shop/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
