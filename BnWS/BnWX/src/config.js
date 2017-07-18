f07ade0e0739021a20cbb750014104145b716d30
sijianbn@hotmail.com <sijianbn@hotmail.com>
2017-07-18 22:23:43 +0800

    xx

--- BnWS/BnWX/src/config.js
+++ BnWS/BnWX/src/config.js
@@ -1,7 +1,7 @@
 export default{
 	// apiServer:'http://sjweb.nat123.net/',
 	// imgServer:'http://sjweb.nat123.net/upload/',
-	apiServer:'http://localhost:7565/',
-	imgServer:'http://localhost:7565/upload/',
+	apiServer:'http://bn.inpar.com.cn/',
+	imgServer:'http://bn.inpar.com.cn/upload/',
 	jsApiList:['chooseWXPay','getLocation','openLocation','chooseImage','getLocalImgData']
 }
